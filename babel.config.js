bfc056ca6a018350f3f482009a6be6b1f3b6974f
ShadyBenjy <shadbenjamim@gmail.com>
2023-06-10 19:38:40 +0300

    Working Update

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-    "presets": ["@babel/preset-flow", "module:metro-react-native-babel-preset"],
-    "plugins": ["@babel/plugin-syntax-flow"]
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin']
   };
+};
